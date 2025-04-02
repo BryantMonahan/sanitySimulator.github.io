032a20a80f46150304747f147eeba351382ca205
BryantMonahan <bryantmonahan@gmail.com>
2025-04-02 13:10:04 -0400

    Added two php files and centered the footers

---
+++ frontend/header.php
@@ -0,0 +1,14 @@
+<header>
+        <title>Money Movement - Auto</title>
+        <img src="/frontend/images/newLogoPng.png" alt="Logo" class="logo">
+        <h1 style="color: #D8315B" class="pageHeader">Money Movement</h1>
+        <nav class="navbar">
+            <ul>
+                <li><a href="home.html">Home</a></li>
+                <li><a href="mortgage.html">Mortgages</a></li>
+                <li><a href="">Credit/Credit Cards</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Feedback</a></li>
+            </ul>
+        </nav>
+    </header>
