e2949dcf606cc44dff6fce411625de7d984a0928
BryantMonahan <bryantmonahan@gmail.com>
2025-04-21 13:29:25 -0400

    Changed font, font color, and background to give a more proffesional stlye

---
+++ frontend/footer.php
@@ -0,0 +1,5 @@
+<footer style="color:black; text-align:center;">
+        <hr>
+        <p class = "footer">Bryant Monahan</p>
+        <p class = "footer">Copyright © 2025</p>
+</footer>
--- frontend/header.php
+++ frontend/header.php
@@ -1,7 +1,7 @@
 <header>
         <title>Money Movement - Auto</title>
         <img src="/frontend/images/newLogoPng.png" alt="Logo" class="logo">
-        <h1 style="color: #D8315B" class="pageHeader">Money Movement</h1>
+        <h1 style="color:rgb(0, 0, 0)" class="pageHeader">Money Movement</h1>
         <nav class="navbar">
             <ul>
                 <li><a href="home.html">Home</a></li>
