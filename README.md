85435c0fdf2ca9669ac9594f9ce21fcfb6315665
BryantMonahan <bryantmonahan@gmail.com>
2025-07-25 15:43:26 -0400

    Updated api calling functions and made it so the csv file updates. New favicon too.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is meant to be a guide for those wanting to learn the basics of personal finance. This site's name is currently undecided as Money Movement 
+This is meant to be a guide for those wanting to learn the basics of personal finance. This site's name is currently undecided as Mooka Money 
 is way too expensive to buy the domain name for. A main focus of this website is to be visually pleasing and for users to enjoy playing around with
 the graphs/calculators in hopes that they gain some interest and pursue more information outside of sanitySimulator.
--- server.js
+++ server.js
@@ -4,43 +4,46 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 require('dotenv').config();
 const fs = require('fs')
+const { Buffer } = require('buffer')
+const XLSX = require('xlsx')
 
-
-
-async function getApiData() {
-    // get the median home sale price
-    let apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=MSPNHSUS&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
-    let response = await fetch(apiURL)
-    let data = await response.json()
-    fs.writeFileSync('./public/JSON_Data/homeSalePrice.json', JSON.stringify(data))
-    // get inflation
-    apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=FPCPITOTLZGUSA&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
-    response = await fetch(apiURL)
-    data = await response.json()
-    fs.writeFileSync('./public/JSON_Data/inflation.json', JSON.stringify(data))
-    // get median income
-    apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=MEHOINUSA646N&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
-    response = await fetch(apiURL)
-    data = await response.json()
-    fs.writeFileSync('./public/JSON_Data/medianIncome.json', JSON.stringify(data))
-    // 60 month auto loan
-    apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=RIFLPBCIANM60NM&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
-    response = await fetch(apiURL)
-    data = await response.json()
-    fs.writeFileSync('./public/JSON_Data/autoRates.json', JSON.stringify(data))
-    // credit card rate
-    apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=TERMCBCCALLNS&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
-    response = await fetch(apiURL)
-    data = await response.json()
-    fs.writeFileSync('./public/JSON_Data/creditCardRates.json', JSON.stringify(data))
-    // 30 year fixed mortgage rate
-    apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=MORTGAGE30US&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
-    response = await fetch(apiURL)
-    data = await response.json()
-    fs.writeFileSync('./public/JSON_Data/mortgageRates.json', JSON.stringify(data))
+async function getHomeAff() {
+    try {
+        let response = await fetch('https://www.atlantafed.org/-/media/documents/research/housing-and-policy/hoam/HOAM_US_Affordability_Index.xlsx')
+        if (!response.ok) throw new Error('Something went wrong fetching the Atlanta Fed Data')
+        const arrayBuffer = await response.arrayBuffer()
+        const buffer = Buffer.from(arrayBuffer)
+        fs.writeFileSync('./buffer.xlsx', buffer)
+        const pages = XLSX.readFile('./buffer.xlsx')
+        const pageName = pages.SheetNames[0]
+        const page = pages.Sheets[pageName]
+        const csv = XLSX.utils.sheet_to_csv(page)
+        fs.writeFileSync('./public/JSON_Data/HOAM_US_Affordability_Index.csv', csv)
+    } catch (err) {
+        console.log('Error: ', err.message)
+    }
 }
 
-getApiData()
+async function getStLouisApiData(series_id, path) {
+    try {
+        let apiURL = `https://api.stlouisfed.org/fred/series/observations?series_id=${series_id}&api_key=${process.env.ST_LOUIS_KEY}&file_type=json`
+        let response = await fetch(apiURL)
+        if (!response.ok) throw new Error(`Something went wrong trying to get: ${series_id}`)
+        let data = await response.json()
+        fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+        console.log('Ran into error', err.message)
+    }
+}
+
+getHomeAff()
+getStLouisApiData('MSPNHSUS', './public/JSON_Data/homeSalePrice.json')
+getStLouisApiData('FPCPITOTLZGUSA', './public/JSON_Data/inflation.json')
+getStLouisApiData('MEHOINUSA646N', './public/JSON_Data/medianIncome.json')
+getStLouisApiData('RIFLPBCIANM60NM', './public/JSON_Data/autoRates.json')
+getStLouisApiData('TERMCBCCALLNS', './public/JSON_Data/creditCardRates.json')
+getStLouisApiData('MORTGAGE30US', './public/JSON_Data/mortgageRates.json')
+
 app.listen(10000, () => {
     console.log('app has started')
 })
